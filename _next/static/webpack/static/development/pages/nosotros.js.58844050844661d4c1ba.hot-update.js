7c91d53aba42af1a6a24d358b59862aee95493dc
Alexander Caravaca <alexander.caravaca1@CR-HER-WKS17816.globalservs.com>
2019-06-11 14:26:17 -0600

    first commit

---
+++ _next/static/webpack/static/development/pages/nosotros.js.58844050844661d4c1ba.hot-update.js
@@ -0,0 +1,65 @@
+webpackHotUpdate("static/development/pages/nosotros.js",{
+
+/***/ "./pages/nosotros.jsx":
+/*!****************************!*\
+  !*** ./pages/nosotros.jsx ***!
+  \****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_Master__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/Master */ "./components/Master.jsx");
+var _jsxFileName = "/Users/alexander.caravaca1/Documents/React-training/Bitcoin-Next/pages/nosotros.jsx";
+
+
+
+var nosotros = function nosotros() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Master__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+    className: "text-center",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, "Sobre Nosotros"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }, "Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent fermentum commodo leo gravida euismod. Quisque mattis nisl nec pellentesque euismod. Nulla a tempor risus, ac elementum libero. Ut mi erat, scelerisque vel dapibus sed, dapibus ac nunc. Aenean ex velit, aliquet eget nisi ut, feugiat pretium felis. Sed gravida augue scelerisque sapien ultrices porttitor. Quisque luctus quam ut scelerisque pellentesque. Cras eget volutpat nisi. Suspendisse porta laoreet lectus euismod pharetra. Quisque vel elementum nunc, rhoncus sodales ante."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, "Nunc sed magna vel elit ultrices pellentesque non sit amet purus. Mauris luctus iaculis orci et tempor. Suspendisse at erat facilisis, dictum turpis eu, ullamcorper mauris. Donec vehicula lacus sit amet tellus elementum, et sodales sem suscipit. Ut maximus vehicula feugiat. Mauris et bibendum libero. Aliquam volutpat consequat ipsum."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, "Vivamus eu fringilla justo, et pulvinar odio. Pellentesque orci ex, porttitor id mattis ut, maximus vel arcu. Nulla eget nisi mi. Praesent eget nisl mauris. Integer gravida sapien congue massa pulvinar, et finibus lorem molestie. Mauris varius sapien ut tincidunt semper. Maecenas ac massa erat. Donec diam ante, laoreet at bibendum vitae, tincidunt nec risus. Donec lacinia ultricies enim in vulputate. Aliquam erat volutpat. Nunc condimentum erat in metus aliquam lacinia. Suspendisse at arcu euismod, tristique magna vel, pellentesque nisi. In hac habitasse platea dictumst. Aliquam vehicula tempor sagittis. Suspendisse potenti. Nam aliquam viverra metus, consectetur tempor dolor pharetra pellentesque.")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (nosotros);
+
+/***/ })
+
+})
+//# sourceMappingURL=nosotros.js.58844050844661d4c1ba.hot-update.js.map
